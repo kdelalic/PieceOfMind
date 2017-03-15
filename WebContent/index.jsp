3048ba1acd91948b7f31e3a0a12ca69f1bf9e5d9
kdelalic <karlo.delalic1@gmail.com>
2017-03-14 23:27:30 -0400

    first commit

---
+++ WebContent/index.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
+    "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <meta name="viewport" content="width=device-width">
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:thin|Roboto" rel="stylesheet" type="text/css"/>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <title>Piece of Mind</title>
+</head>
+<body>
+	<header>
+        <h1>Piece of Mind</h1>
+        <small>A web app to check the mental state of Twitter users.</small>
+        <div class="d5">
+            <form method="get" action="search">
+                <input type="text" name="username" placeholder="Enter a userID...">
+                <button type="submit" action="#" ></button>
+            </form>
+        </div>
+    </header>
+<% java.util.Date d = new java.util.Date(); %>
+<h1>
+Today's date is <%= d.toString() %> and this jsp page worked!
+</h1>
+</body>
+</html>
---
+++ src/com/example/servlets/HelloServlet.java
@@ -0,0 +1,64 @@
+package com.example.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/search")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HelloServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		String username = request.getParameter("username");
+
+		try {
+			out.println("<!DOCTYPE html>");
+			out.println("<html><head>");
+			out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+			out.println("<title>" + username + "</title></head>");
+
+
+			out.println("<p>Name: " + username + "</p>");
+
+			out.println("</body></html>");
+		} finally {
+			out.close();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
