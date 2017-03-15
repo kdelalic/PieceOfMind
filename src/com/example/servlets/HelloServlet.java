041ff05d2cbbf2ad78003b3229da06a773c05897
kdelalic <karlo.delalic1@gmail.com>
2017-03-14 23:40:51 -0400

    add search bar to results page

--- src/com/example/servlets/HelloServlet.java
+++ src/com/example/servlets/HelloServlet.java
@@ -2,7 +2,6 @@ package com.example.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,7 +21,6 @@ public class HelloServlet extends HttpServlet {
 	 */
 	public HelloServlet() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
@@ -31,17 +29,34 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		String username = request.getParameter("username");
 
 		try {
 			out.println("<!DOCTYPE html>");
-			out.println("<html><head>");
-			out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+			out.println("<meta name='viewport' content='width=device-width'>");
+			out.println("<meta charset='utf-8'>");
 			out.println("<title>" + username + "</title></head>");
-
+			out.println(
+					"<link href='https://fonts.googleapis.com/css?family=Raleway:thin|Roboto' rel='stylesheet' type='text/css'/>");
+			out.println("<link href='css/style.css' rel='stylesheet' type='text/css'>");
+			out.println(
+					"<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'>");
+			out.println("<title>Piece of Mind</title>");
+			out.println("</head>");
+			out.println("<body>");
+			out.println("<header>");
+			out.println("<h1>Piece of Mind</h1>");
+			out.println("<small>A web app to check the mental state of Twitter users.</small>");
+			out.println("<div class='d5'>");
+			out.println("<form method='get' action='search'>");
+			out.println("<input type='text' name='username' placeholder='Enter a userID...'>");
+			out.println("<button type='submit' action='#' ></button>");
+			out.println("</form>");
+			out.println("</div>");
+			out.println("</header>");
 
 			out.println("<p>Name: " + username + "</p>");
 
@@ -57,7 +72,6 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
